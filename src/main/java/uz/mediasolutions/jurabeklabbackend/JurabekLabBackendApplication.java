6a7c6dbe844309eab337cc6ec9c287fcfb92a1d9
KomilovAzizbekk <komilovazizbek.ppp@gmail.com>
2024-09-04 01:52:36 +0500

    Started. Bismillah!

---
+++ src/main/java/uz/mediasolutions/jurabeklabbackend/JurabekLabBackendApplication.java
@@ -0,0 +1,13 @@
+package uz.mediasolutions.jurabeklabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurabekLabBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JurabekLabBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/mediasolutions/jurabeklabbackend/JurabekLabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.mediasolutions.jurabeklabbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurabekLabBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
