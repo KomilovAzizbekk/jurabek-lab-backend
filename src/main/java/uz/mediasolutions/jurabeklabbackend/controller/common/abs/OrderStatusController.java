64eeaff820710aac3cf2c628159771735a6f893f
KomilovAzizbekk <komilovazizbek.ppp@gmail.com>
2024-09-08 02:59:05 +0500

    yol togirlandi

--- src/main/java/uz/mediasolutions/jurabeklabbackend/controller/common/abs/OrderStatusController.java
+++ src/main/java/uz/mediasolutions/jurabeklabbackend/controller/common/abs/OrderStatusController.java
@@ -2,13 +2,13 @@ package uz.mediasolutions.jurabeklabbackend.controller.common.abs;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import uz.mediasolutions.jurabeklabbackend.enums.OrderStatus;
 import uz.mediasolutions.jurabeklabbackend.utills.constants.Rest;
 
 import java.util.List;
 
-@RestController(Rest.BASE_PATH + "order-statuses")
+@RequestMapping(Rest.BASE_PATH + "order-statuses")
 public interface OrderStatusController {
 
     @GetMapping
